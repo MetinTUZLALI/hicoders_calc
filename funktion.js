26afa53ee9c1d6fbb728e182e220ed50924c9456
unknown <metintuzlali@gmail.com>
2021-03-07 19:28:08 +0100

    eklendi

---
+++ funktion.js
@@ -0,0 +1,26 @@
+
+
+function add(x,y) {
+    let sum = x+y;
+    return sum;
+}
+
+
+function sub(x,y) {
+    let sub = x-y;
+    return sub;
+}
+
+
+function mult(x,y) {
+    let mult = x*y;
+    return mult;
+}
+
+
+function div(x,y) {
+    let div = x/y;
+    return div;
+}
+
+modul.export={add,sub,mult,div};
---
+++ index.js
@@ -0,0 +1,7 @@
+const x = 4
+const y = 8
+
+const {add,sub,mult,div} = require ("./funktion");
+
+console.log(sub(x,y));
+console.log("add  :", functions.sum(20, 4));
